54edf34f9edfe1b6a395ba6a33f9996f371f3fff
matthew-c-lee <77367175+matthew-c-lee@users.noreply.github.com>
2022-09-19 19:31:47 -0400

    SPN

---
+++ ciphers/SPN/testSPN.py
@@ -0,0 +1,53 @@
+import SPNencrypt
+
+def main():
+    
+    plain = "0010011010110111"
+    SPNencrypt.spn(plain)
+    
+    plain = "1111111111111111"
+    SPNencrypt.spn(plain)
+    
+    plain = "0000000000000000"
+    SPNencrypt.spn(plain)
+    
+    plain = "1111111111111110"
+    SPNencrypt.spn(plain)
+    
+    plain = "1110111111011110"
+    SPNencrypt.spn(plain)
+    
+    plain = "1110101011011110"
+    SPNencrypt.spn(plain)
+
+    plain = "1110101000011110"
+    SPNencrypt.spn(plain)
+    
+    plain = "0101010101010101"
+    SPNencrypt.spn(plain)
+    
+    plain = "0101010101010101"
+    SPNencrypt.spn(plain)
+    
+    plain = "0101000000000101"
+    SPNencrypt.spn(plain)
+    
+    plain = "0101000010100101"
+    SPNencrypt.spn(plain)
+    
+    plain = "1110111111110101"
+    SPNencrypt.spn(plain)
+    
+    plain = "0000000100001000"
+    SPNencrypt.spn(plain)
+    
+    plain = "0001000100001001"
+    SPNencrypt.spn(plain)
+    
+    plain = "0110111001110111"
+    SPNencrypt.spn(plain)
+    
+    plain = "1111011111011101"
+    SPNencrypt.spn(plain)
+    
+main()
